52af9a3e8e06752448fe8526136cecbd86f5ac1a
wellifabio <wellifabio@hotmail.com>
2024-10-24 08:18:22 -0300

    swagger funcionarios e animais

---
+++ server.js
@@ -0,0 +1,24 @@
+const jsonServer    = require("json-server")
+const path          = require("path")
+const fs            = require("fs")
+const express       = require("express")
+
+const server        = jsonServer.create()
+const router        = jsonServer.router("db.json")
+
+// Documentação Swagger
+const swaggerUi     = require('swagger-ui-express');
+const swaggerDoc    = require('./swagger.json'); // Arquivo de documentação Swagger
+server.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+const port = 3000;
+
+server.db = router.db
+server.use(router)
+
+server.listen(port, () => {
+    console.log("\x1b[36m%s\x1b[0m", "JSON Server executando na porta: " + port)
+    console.log("\x1b[1m%s\x1b[0m", "\nRecursos disponíveis: \n")
+    console.log("http://localhost:3000/swagger \n")
+    Object.keys(router.db.__wrapped__).forEach( recurso => console.log(`http://localhost:${port}/${recurso}`) )
+})
